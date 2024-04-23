7ee4afdcd21816237fe2a5fd1c6d337033a9f132
faracodes <faramadeifat@gmail.com>
2024-04-23 16:35:29 +0100

    updated services

--- backend/services/api.go
+++ backend/services/api.go
@@ -1,1 +1,33 @@
 package services
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"main.go/models"
+)
+
+// FetchTarotCards makes a GET request to the API to fetch tarot cards
+// and returns a slice of Card structs representing the cards.
+func FetchTarotCards() ([]models.Card, error) {
+
+	apiUrl := "https://tarotapi.dev/api/v1/cards"
+
+	// Send GET request to the API
+	resp, err := http.Get(apiUrl)
+	if err != nil {
+		return nil, fmt.Errorf("failed to make GET request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Decode JSON response into a slice of Card structs
+	var cardsResponse struct {
+		Cards []models.Card `json:"cards"`
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&cardsResponse); err != nil {
+		return nil, fmt.Errorf("failed to decode JSON response: %v", err)
+	}
+
+	return cardsResponse.Cards, nil
+}
