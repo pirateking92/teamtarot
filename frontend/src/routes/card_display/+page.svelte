513f40a0e4a59106d0d53090864cdacac3d70cdd
loopdom <giandomenicoloparco@gmail.com>
2024-04-25 12:14:56 +0100

    Changes to display_card page styling

--- frontend/src/routes/card_display/+page.svelte
+++ frontend/src/routes/card_display/+page.svelte
@@ -1,13 +1,10 @@
-<!-- 
-    this script text is the js that parses the JSON data.
-Tried to change routes and get a response, but didnt get very far.
-Perhaps some changes to the routes? Tried getting responses from the backend but couldnt.
-Code has been changed in routes package, some added to main
--->
-<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-
 <script>
   import { onMount } from "svelte";
+  import { userName } from "../stores.js";
+
+  function handleButtonClick() {
+    window.location.href = "/card_display";
+  }
 
   let threeCards = [];
   let isLoading = true;
@@ -29,11 +26,24 @@ Code has been changed in routes package, some added to main
     }
   });
 </script>
+
+<!-- 
+    this script text is the js that parses the JSON data.
+Tried to change routes and get a response, but didnt get very far.
+Perhaps some changes to the routes? Tried getting responses from the backend but couldnt.
+Code has been changed in routes package, some added to main
+-->
+<link
+  href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
+  rel="stylesheet"
+/>
 <div class="container mx-auto mt-8">
   <div class="flex justify-between items-center mb-8">
-    <h1 class="text-4xl font-bold">Arcana Tarot Card Reading</h1>
-    <a href="/"class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
-      >Give me another reading</a
+    <h1 class="text-4xl font-bold card-display-title">
+      Arcana Tarot Card Reading
+    </h1>
+    <button on:click={handleButtonClick} style="--clr:#c377d4"
+      ><span>Draw new Tarot Cards</span><i></i></button
     >
   </div>
 
@@ -42,45 +52,172 @@ Code has been changed in routes package, some added to main
   {:else if error}
     <p class="text-red-500">Error: {error}</p>
   {:else}
-  <div class="mx-auto px-20"> 
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 ml-12 mr-12">
-      {#each threeCards as card, index}
-        <div class="bg-white shadow-md p-4 rounded-lg">
-          <h2 class="text-xl font-semibold mb-2">
-            {#if index === 0}
-              <span>Past</span>
-            {:else if index === threeCards.length - 1}
-              <span>Future</span>
+    <div class="mx-auto px-20">
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-4 ml-12 mr-12">
+        {#each threeCards as card, index}
+          <div class="bg-white shadow-md p-4 rounded-lg">
+            <h2 class="text-xl font-semibold mb-2">
+              {#if index === 0}
+                <span>Past</span>
+              {:else if index === threeCards.length - 1}
+                <span>Future</span>
+              {:else}
+                <span>Present</span>
+              {/if}
+            </h2>
+
+            <h2 class="text-xl font-semibold mb-2">{card.name}</h2>
+            {#if card.reversed}
+              <img
+                src={`src/lib/assets/${card.image_file_name}`}
+                alt={card.name}
+                class="mb-2 rounded-lg card-image"
+                style="transform: rotate(180deg);"
+              />
             {:else}
-              <span>Present</span>
+              <img
+                src={`src/lib/assets/${card.image_file_name}`}
+                alt={card.name}
+                class="mb-2 rounded-lg card-image"
+              />
             {/if}
-          </h2>
-          
-          <h2 class="text-xl font-semibold mb-2">{card.name}</h2>
-          {#if card.reversed}
-            <img src={`src/lib/assets/${card.image_file_name}`} alt="{card.name}" class="mb-2 rounded-lg card-image" style="transform: rotate(180deg);">
-          {:else}
-            <img src={`src/lib/assets/${card.image_file_name}`} alt="{card.name}" class="mb-2 rounded-lg card-image">
-          {/if}
-          <p class="text-gray-600 mb-2">Type: {card.type}</p>
-          {#if card.reversed}
-            <p class="text-gray-600 mb-2">Meaning Reversed: {card.meaning_rev}</p>
-          {:else}
-            <p class="text-gray-600 mb-2">Meaning Upright: {card.meaning_up}</p>
-          {/if}
-        </div> 
-      {/each}
+            <p class="text-gray-600 mb-2">Type: {card.type}</p>
+            {#if card.reversed}
+              <p class="text-gray-600 mb-2">
+                Meaning Reversed: {card.meaning_rev}
+              </p>
+            {:else}
+              <p class="text-gray-600 mb-2">
+                Meaning Upright: {card.meaning_up}
+              </p>
+            {/if}
+          </div>
+        {/each}
+      </div>
     </div>
-  </div>
   {/if}
-  <div class="flex justify-center">
-  </div>
+  <div class="flex justify-center"></div>
 </div>
 
-
 <style>
+  @import url("https://fonts.googleapis.com/css2?family=MedievalSharp&display=swap");
+
+  :global(html, body) {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    background: linear-gradient(to right, #20051c, #46204e);
+  }
+
+  .card-display-title {
+    color: whitesmoke;
+    font-family: MedievalSharp, Arial, Helvetica, sans-serif;
+  }
+
   .card-image {
     max-width: 200px; /* Set the desired maximum width */
     max-height: 300px; /* Set the desired maximum height */
+  }
+
+  button {
+    position: relative;
+    background: #1d1242;
+    color: #fff;
+    text-decoration: none;
+    text-transform: uppercase;
+    border: none;
+    letter-spacing: 0.2rem;
+    font-size: 1.5rem;
+    padding: 1.3rem 3rem;
+    transition: 0.2s;
+    animation: box 2s infinite;
+    border-radius: 20px;
+  }
+
+  button:hover {
+    letter-spacing: 0.3rem;
+    padding: 1.3rem 3rem;
+    background: var(--clr);
+    color: var(--clr);
+    /* box-shadow: 0 0 35px var(--clr); */
+    animation: box 1.5s infinite;
+  }
+
+  button::before {
+    content: "";
+    position: absolute;
+    inset: 2px;
+    background: linear-gradient(to right, #20051c, #46204e);
+    border-radius: 20px;
+  }
+
+  button span {
+    position: relative;
+    z-index: 1;
+  }
+  button i {
+    position: absolute;
+    inset: 0;
+    display: block;
+  }
+
+  button i::before {
+    content: "";
+    position: absolute;
+    width: 10px;
+    height: 2px;
+    left: 80%;
+    top: -2px;
+    border: 2px solid var(--clr);
+    background: #272822;
+    transition: 0.2s;
+  }
+
+  button:hover i::before {
+    width: 15px;
+    left: 20%;
+    animation: move 3s infinite;
+  }
+
+  button i::after {
+    content: "";
+    position: absolute;
+    width: 10px;
+    height: 2px;
+    left: 20%;
+    bottom: -2px;
+    border: 2px solid var(--clr);
+    background: #272822;
+    transition: 0.2s;
+  }
+
+  button:hover i::after {
+    width: 15px;
+    left: 80%;
+    animation: move 3s infinite;
+  }
+
+  @keyframes move {
+    0% {
+      transform: translateX(0);
+    }
+    50% {
+      transform: translateX(5px);
+    }
+    100% {
+      transform: translateX(0);
+    }
+  }
+
+  @keyframes box {
+    0% {
+      box-shadow: #27272c;
+    }
+    50% {
+      box-shadow: 0 0 25px var(--clr);
+    }
+    100% {
+      box-shadow: #27272c;
+    }
   }
 </style>
