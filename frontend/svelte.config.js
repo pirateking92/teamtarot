493e6eb04b3df0cb5f486f4e978825d6a6b8a867
Matt D <doyle9214@gmail.com>
2024-05-01 11:16:38 +0100

    more testing. first deploy fail

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,21 +1,19 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-        // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter()
     },
-    preprocess: vitePreprocess()
+    vite: {
+        plugins: [
+            svelte({
+                preprocess: sveltePreprocess({
+                    postcss: true
+                })
+            })
+        ]
+    }
 };
-
-export default {
-    ...config,
-    preprocess: sveltePreprocess({
-        postcss: true
-    })
-};
+export default config;
