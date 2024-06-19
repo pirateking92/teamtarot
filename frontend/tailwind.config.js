e7812c68d59e1dc9880d7b1e6f5811cde1dda8f8
Matt Doyle <128827327+pirateking92@users.noreply.github.com>
2024-06-19 16:29:36 +0100

    Delete frontend/tailwind.config.js
    removed because not being used
    
    Signed-off-by: Matt Doyle <128827327+pirateking92@users.noreply.github.com>

--- frontend/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['/Users/olikelly/Documents/MakersCode/team_tarot/frontend/src/routes/+page.svelte'],
-  theme: {
-    extend: {}, 
-  },
-  plugins: [],
-}
